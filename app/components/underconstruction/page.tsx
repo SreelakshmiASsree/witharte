8393ac66aca91af78bc70d425849bf0bc25cdbc0
sreelakshmi <you@example.com>
2025-11-14 16:55:52 +0530

    minor position corrected

--- app/components/underconstruction/page.tsx
+++ app/components/underconstruction/page.tsx
@@ -12,18 +12,18 @@ export default function Underconstruction() {
                 <div className="absolute inset-0 flex flex-col items-center justify-center z-10 text-white">
                     <div className="relative">
                         <Image src={logo} alt="logo" className="lg:w-3xl w-2xl object-contain" />
-                        <div className="absolute lg:top-80 lg:left-80  top-[155px] left-[135px]">
-                            <p className="text-center lg:text-5xl text-3xl font-extralight">Gallery</p>
+                        <div className="absolute lg:top-80 lg:left-80  top-[155px] left-[87px]">
+                            <p className="text-center lg:text-5xl text-2xl font-extralight">Gallery</p>
                         </div>
                     </div>
                     <div className="absolute lg:bottom-20 bottom-24">
                         <p className=" lg:text-lg text-sm font-normal tracking-widest">WE  ARE  PUBLISHING  SOON...</p>
                     </div>
-                    <div className="flex gap-5 absolute lg:bottom-10 bottom-14">
+                    <div className="flex items-center lg:gap-5 gap-3 absolute lg:bottom-10 bottom-14">
                         <a href="https://www.instagram.com/witharte_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
-                            <FaInstagram className="md:text-3xl text-2xl text-[#ffffff]" />
+                            <FaInstagram className="md:text-3xl text-xl text-[#ffffff]" />
                         </a>
-                        <p className="text-[#ffffff] md:text-2xl text-xl font-medium flex items-center gap-1">
+                        <p className="text-[#ffffff] md:text-2xl text-lg font-medium flex items-center gap-1">
                             <span><FiPhoneCall /></span>
                             <a href=""></a>+91 9633249362
                         </p>
