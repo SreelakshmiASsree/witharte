df99ab88c8b612f50dd6d2e232fe6ab6c82e8349
sreelakshmi <you@example.com>
2025-11-14 15:46:08 +0530

    image and content changed

--- app/components/underconstruction/page.tsx
+++ app/components/underconstruction/page.tsx
@@ -7,21 +7,19 @@ export default function Underconstruction() {
     return (
         <>
             <div className="relative w-full h-screen">
-                <Image
-                    src={bgimage}
-                    alt="Background"
-                    fill
-                    className="object-cover"
-                />
+                
                 {/* Your content on top */}
                 <div className="absolute inset-0 flex flex-col items-center justify-center z-10 text-white">
                     <div className="relative">
                         <Image src={logo} alt="logo" className="lg:w-3xl w-2xl object-contain" />
+                       <div className="absolute lg:top-80 lg:left-80 top-36 left-28">
+                         <p className="text-center lg:text-5xl text-3xl font-extralight">Gallery</p>
+                       </div>
                     </div>
-                    <div className="absolute lg:bottom-56 bottom-60">
+                    <div className="absolute lg:bottom-20 bottom-24">
                         <p className=" lg:text-lg text-sm font-normal tracking-widest">WE  ARE  PUBLISHING  SOON...</p>
                     </div>
-                    <div className="flex gap-5 ">
+                    <div className="flex gap-5 absolute lg:bottom-10 bottom-14">
                 <a href="https://www.instagram.com/witharte_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
                     <FaInstagram  className="md:text-3xl text-2xl text-[#ffffff]" />
                 </a>
